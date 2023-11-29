cc6032731b094b16187293204a7baa727bf6d9a0
Bill Showers <william.showers@ucdenver.edu>
2023-11-28 17:33:10 -0700

    Add DGE functionality for BPCells
    Add code for BPCells in the run_dge.seurat method. The marker_features function is used, and the output is modified for consistency with the DGE tables produced from other methods.

--- R/run_dge.R
+++ R/run_dge.R
@@ -88,47 +88,186 @@ run_dge.Seurat <-
     # Seurat assay to use: "RNA" if not specified
     seurat_assay <- seurat_assay %||% "RNA"
 
-    # Run presto
-    dge_table <-
-      presto::wilcoxauc(
-        AML_Seurat,
-        group_by = group_by,
-        assay = slot,
-        seurat_assay = seurat_assay
-      )
-
-    # Convert to tibble, remove wilcoxon rank sum U statistic
-    dge_table <-
-      dge_table %>%
-      as_tibble() %>%
-      dplyr::select(-statistic) %>%
-      # Remove p-value column if specified (only shows adjusted p-value)
-      {if (remove_raw_pval == TRUE) dplyr::select(., -pval) else .} %>%
-      # Return only genes that are upregulated in each group
-      # if positive_only is TRUE
-      {if (positive_only == TRUE) dplyr::filter(., logFC > 0) else .} %>%
-      # Sort by group, then by adjusted p-value, then by descending LFC
-      dplyr::arrange(group, padj, desc(abs(logFC)))
-
-    # Convert to log2FC format if desired by user
-    if (lfc_format == "log2"){
-      dge_table$logFC <-
-        scDE:::ln_to_log2(dge_table$logFC)
-
-      # Rename column to specify log2FC values
-      dge_table <-
-        dge_table %>%
-        dplyr::rename(Log2FC = logFC)
+    # If matrix at the defined slot is a BPCells matrix, use the BPCells
+    # marker_feature function.
+    # If a dGC or dense matrix, use presto.
+    # Code to test class of expression matrix depends on the structure of the
+    # assay (v3 vs. v5)
+    if (inherits(object[[seurat_assay]], "Assay5")){
+      if (inherits(object[[seurat_assay]]@layers[[slot]], "IterableMatrix")){
+        test_use <- "BPCells"
+      } else {
+        test_use <- "Presto"
+      }
+    } else if (inherits(object[[seurat_assay]], "Assay")){
+      # Uses single brackets to pull via @ (@counts or @data)
+      if (inherits(object[[seurat_assay]][slot], "IterableMatrix")){
+        test_use <- "BPCells"
+      } else {
+        test_use <- "Presto"
+      }
+    } else {
+      stop('Unsupported assay class for assay "', seurat_assay, '".')
     }
 
-    # Rename adjusted p-value column to pval_adj
-    dge_table <-
-      dge_table %>%
-      dplyr::rename(pval_adj = padj)
+    if (test_use == "BPCells"){
+      # marker_features requires vector of group labels by cell
+      groups <-
+        SCUBA::fetch_metadata(
+          object,
+          vars = group_by,
+          return_class = "vector"
+          )
 
-    # Put group before feature column
-    dge_table <-
-      dplyr::relocate(dge_table, group, .before = feature)
+      # Run BPCells marker_features
+      dge_table <-
+        BPCells::marker_features(
+          mat = object[[seurat_assay]]@layers[[slot]],
+          groups = groups,
+          method = "wilcoxon"
+          )
+
+      # Post-DGE modifications to table
+      # Compute LFC based on log scaling format (ln or log2)
+      if (lfc_format == "log2"){
+        dge_table <-
+          dge_table |>
+          dplyr::mutate(
+            log2FC =
+              # Calculation depends on if the matrix used has been
+              # natural log-transformed. This is the case for all slots
+              # except counts
+              if (slot == "data"){
+                # Natural log-transformed matrices: subtract ln-transformed
+                # means and divide by ln(2) to convert to log2 format
+                (foreground_mean - background_mean)/log(2)
+              } else {
+                # For non-transformed matrices, compute a fold-change
+                # in log-2 format
+                log2(foreground_mean/background_mean)
+              }
+          )
+      } else if (lfc_format == "ln"){
+        dge_table <-
+          dge_table |>
+          dplyr::mutate(
+            logFC =
+              # Calculation depends on if the matrix used has been
+              # natural log-transformed. This is the case for all slots
+              # except counts
+              if (slot == "data"){
+                # Natural log-transformed matrices:
+                # subtract ln-transformed means
+                # foreground/background = ln(foreground) - ln(background)
+                foreground_mean - background_mean
+              } else {
+                # For non-transformed matrices, take the natural log
+                # in the fold change of the means
+                log(foreground_mean/background_mean)
+              }
+          )
+      }
+
+      # dge_table <-
+      #   dge_table |>
+      #   dplyr::mutate(
+      #     # Average expression in group: convert to log-2 form?
+      #     # (division is by ln(2))
+      #     avgExpr = foreground_mean/log(2),
+      #   )
+
+      # Return only genes that are upregulated in each group if positive_only
+      # is TRUE
+      # Filter based on LFC or L2FC column
+      if (remove_raw_pval == TRUE){
+        dge_table <-
+          dge_table %>%
+          {if (lfc_format == "log2") dplyr::filter(., log2FC > 0)
+            else if (lfc_format == "ln") dplyr::filter(., logFC > 0)}
+        }
+
+      # Additional modifications
+      dge_table <-
+        dge_table %>%
+        # Remove raw p-value column if specified (only shows adjusted p-value)
+        {if (remove_raw_pval == TRUE) dplyr::select(., -p_val_raw) else .} %>%
+        dplyr::mutate(
+          # Benjamini-Hochberg p-value correction
+          padj = stats::p.adjust(p_val_raw, method = "BH"),
+          # Add NA entries for pct_in, pct_out
+          # These are not computed by marker_features. We will include these
+          # values in acknowledgement of their utility, and presence in other
+          # DGE functions
+          pct_in = NA_character_,
+          pct_out = NA_character_
+        ) %>%
+        dplyr::select(
+          -any_of(c("background", "foreground_mean", "background_mean"))
+          )
+
+      # Rename columns (old = new pairs)
+      rename_cols <-
+        c("group" = "foreground",
+          "pval" = "p_val_raw",
+          "avgExpr" = "foreground_mean"
+        )
+
+      dge_table <-
+        dge_table %>%
+        dplyr::rename(
+          any_of(rename_cols)
+          ) %>%
+        # Also, move columns for consistency with output of other methods
+        dplyr::relocate(
+          c(avgExpr, log2FC, pval, padj),
+          .after = feature
+        ) %>%
+        # Sort by group, then by adjusted p-value, then by descending LFC
+        {if (lfc_format == "log2") dplyr::arrange(group, padj, desc(abs(log2FC)))
+          else if (lfc_format == "ln") dplyr::arrange(group, padj, desc(abs(logFC)))}
+    } else if (test_use == "Presto"){
+      # Run presto
+      dge_table <-
+        presto::wilcoxauc(
+          AML_Seurat,
+          group_by = group_by,
+          assay = slot,
+          seurat_assay = seurat_assay
+        )
+
+      # Convert to tibble, remove wilcoxon rank sum U statistic
+      dge_table <-
+        dge_table %>%
+        as_tibble() %>%
+        dplyr::select(-statistic) %>%
+        # Remove p-value column if specified (only shows adjusted p-value)
+        {if (remove_raw_pval == TRUE) dplyr::select(., -pval) else .} %>%
+        # Return only genes that are upregulated in each group
+        # if positive_only is TRUE
+        {if (positive_only == TRUE) dplyr::filter(., logFC > 0) else .} %>%
+        # Sort by group, then by adjusted p-value, then by descending LFC
+        dplyr::arrange(group, padj, desc(abs(logFC)))
+
+      # Convert to log2FC format if desired by user
+      if (lfc_format == "log2"){
+        dge_table$logFC <-
+          scDE:::ln_to_log2(dge_table$logFC)
+
+        # Rename column to specify log2FC values
+        dge_table <-
+          dge_table %>%
+          dplyr::rename(Log2FC = logFC)
+      }
+
+      # Rename adjusted p-value column to pval_adj
+      dge_table <-
+        dge_table %>%
+        dplyr::rename(pval_adj = padj)
+
+      # Put group before feature column
+      dge_table <-
+        dplyr::relocate(dge_table, group, .before = feature)
+    }
 
     dge_table
   }
